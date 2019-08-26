a9e2e3432c01cfab956d0fec3fb0f6b31a906212
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-08-26 12:38:23 -0400

    add deleteBookmarkByID to store

--- scripts/store.js
+++ scripts/store.js
@@ -6,27 +6,36 @@ const store = function(){
   const addBookmark = function(bookmark = Object){
     this.bookmarks.push(bookmark);
   };
+  
   // update a bookmark found by id
   const updateBookmark = function(id, options){
     let bookmark = this.bookmarks.find(bookmark => id === bookmark.id);
     Object.assign(bookmark, options);
   };
+  
   // delete a bookmark by id
+  const deleteBookmarkByID = function(id){
+    this.bookmarks = this.bookmarks.filter(bookmark => id !== bookmark.id);
+  };
+
   // set filter to new value
   // toggle adding form to display for adding new bookmark
   // toggle editing to render editing form for one bookmark
   return {
     bookmarks: [
-      {id: 'foobar', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.'}
+      {id: 'foobar1', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.'},
+      {id: 'foobar2', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.'},
+      {id: 'foobar3', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.'}
     ],
     adding: false,
     editing: false,
     filter: 1,
     addBookmark,
-    updateBookmark
+    updateBookmark,
+    deleteBookmarkByID
   };
 }();
 
-console.log(store);
-// store.updateBookmark('foobar', {title: 'swag update', desc: 'way better updated desc.'});
-console.log(store);
+console.log(store.bookmarks);
+store.deleteBookmarkByID('foobar2');
+console.log(store.bookmarks);
