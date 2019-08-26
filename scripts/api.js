bb71e274e01b6be87f5d9e8e96741dd987b09da7
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-08-26 12:05:29 -0400

    get CRUD working for api.js

--- scripts/api.js
+++ scripts/api.js
@@ -4,18 +4,52 @@
 const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jamesj-bookmarks-app';
 
+  // create a fetch promise for a POST request
+  const createBookmark = function(bookmark = Object){
+    // bookmark.title and bookmark.url required, 
+    // bookmark.desc and bookmark.rating optional
+    return fetch(`${BASE_URL}/bookmarks`, 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bookmark)
+      });
+  };
+
   // return a fetch promise for a GET request
   const getBookmarks = function(){
     return fetch(`${BASE_URL}/bookmarks`);
   };
 
+  // return a fetch promise for a PATCH request
+  const updateBookmark = function(id, options){
+    return fetch(`${BASE_URL}/bookmarks/${id}`, 
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(options)
+      });
+  };
+
   // delete bookmark
-  const deleteBookmark = function(id, options){
-    return fetch(`${BASE_URL}/bookmarks/${id}, ${options}`);
+  const deleteBookmark = function(id){
+    return fetch(`${BASE_URL}/bookmarks/${id}`, 
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
   };
   
   return {
+    createBookmark,
     getBookmarks,
+    updateBookmark,
     deleteBookmark
   };
 })();
