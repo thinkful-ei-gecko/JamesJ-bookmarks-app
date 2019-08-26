760a22ea13f3e96b58b7a51121d1478aa49a8570
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-08-26 12:47:28 -0400

    move editing prop to be on each bookmark, not in top of store obj - implement store methods

--- scripts/store.js
+++ scripts/store.js
@@ -18,20 +18,39 @@ const store = function(){
     this.bookmarks = this.bookmarks.filter(bookmark => id !== bookmark.id);
   };
 
+  // toggle editing to render editing form for one bookmark at a time
+  const toggleEditBookmark = function(id){
+    this.bookmarks.forEach(bookmark => {
+      bookmark.editing = false;
+    });
+    let bookmark = this.bookmarks.find(bookmark => id === bookmark.id);
+    bookmark.editing = !bookmark.editing;
+  };
+
   // set filter to new value
+  const updateRatingFilter = function(num = Number){
+    this.filter = num;
+  };
+
   // toggle adding form to display for adding new bookmark
-  // toggle editing to render editing form for one bookmark
+  const toggleAddBookmarkForm = function(){
+    this.adding = !this.adding;
+  };
+
   return {
     bookmarks: [
-      {id: 'foobar1', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.'},
-      {id: 'foobar2', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.'},
-      {id: 'foobar3', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.'}
+      {id: 'foobar1', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.', editing: false},
+      {id: 'foobar2', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.', editing: false},
+      {id: 'foobar3', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.', editing: false}
     ],
     adding: false,
     editing: false,
     filter: 1,
     addBookmark,
     updateBookmark,
-    deleteBookmarkByID
+    deleteBookmarkByID,
+    updateRatingFilter,
+    toggleAddBookmarkForm,
+    toggleEditBookmark
   };
 }();
