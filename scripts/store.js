7442af4b7dfbfc883e75dcb10ef5f65c541946c9
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-08-26 12:30:40 -0400

    add updateBookmark to store

--- scripts/store.js
+++ scripts/store.js
@@ -7,18 +7,26 @@ const store = function(){
     this.bookmarks.push(bookmark);
   };
   // update a bookmark found by id
+  const updateBookmark = function(id, options){
+    let bookmark = this.bookmarks.find(bookmark => id === bookmark.id);
+    Object.assign(bookmark, options);
+  };
   // delete a bookmark by id
   // set filter to new value
   // toggle adding form to display for adding new bookmark
   // toggle editing to render editing form for one bookmark
   return {
-    bookmarks: [],
+    bookmarks: [
+      {id: 'foobar', title: 'Test title', url: 'http://someplace.com', rating: 4, desc: 'Excellent desc.'}
+    ],
     adding: false,
     editing: false,
     filter: 1,
-    addBookmark
+    addBookmark,
+    updateBookmark
   };
 }();
 
-store.addBookmark({title: 'flip', url: 'http://test.com', rating: 3, desc: 'some testing'});
+console.log(store);
+// store.updateBookmark('foobar', {title: 'swag update', desc: 'way better updated desc.'});
 console.log(store);
