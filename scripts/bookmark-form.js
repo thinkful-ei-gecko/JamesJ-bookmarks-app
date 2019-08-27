d9ed279ca389321e0089ff8203c0e947192b625a
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-08-27 16:08:04 -0400

    More styles, replace number rating with stars in UI

--- scripts/bookmark-form.js
+++ scripts/bookmark-form.js
@@ -8,19 +8,19 @@ const bookmarkForm = function(){
   const render = function(){
     const form = `
       <form id="bookmark-form">
-        <label for="title">Title: </label>
+        <label for="title">Title</label>
         <input type="text" name="title" id="title" placeholder="My Awesome Site" required />
-        <label for="url">Bookmark URL: </label>
+        <label for="url">Bookmark URL</label>
         <input type="url" name="url" id="url" placeholder="http://site.com" required />
-        <label for="desc">Description: </label>
-        <input type="text" name="desc" id="desc" placeholder="Enter a summary for your bookmark"/>
-        <label for="rating">My Rating (1 - 5): </label>
+        <label for="desc">Description</label>
+        <input type="text" name="desc" id="desc" placeholder="Enter a summary"/>
+        <label for="rating">My Rating</label>
         <select name="rating" id="rating" min="1" max="5">
-          <option value="5">5</option>
-          <option value="4">4</option>
-          <option value="3">3</option>
-          <option value="2">2</option>
-          <option value="1">1</option>
+          <option value="5">&#x2605&#x2605&#x2605&#x2605&#x2605</option>
+          <option value="4">&#x2605&#x2605&#x2605&#x2605&#9734;</option>
+          <option value="3">&#x2605&#x2605&#x2605&#9734;&#9734;</option>
+          <option value="2">&#x2605&#x2605&#9734;&#9734;&#9734;</option>
+          <option value="1">&#x2605&#9734;&#9734;&#9734;&#9734;</option>
         </select>
         <button type="submit">Add</button>
         <button type="reset">Cancel</button>
--- scripts/bookmark-item.js
+++ scripts/bookmark-item.js
@@ -6,6 +6,26 @@ const bookmarkItem = function(){
   // produce the HTML needed to render a bookmark within the DOM
 
   const createBookmarkHTML = function(obj){
+    let stars = '';
+    switch (obj.rating) {
+    case 1:
+      stars = '&#x2606;&#x2606;&#x2606;&#x2606;&#x2605';
+      break;
+    case 2:
+      stars = '&#x2606;&#x2606;&#x2606;&#x2605;&#x2605';
+      break;
+    case 3:
+      stars = '&#x2606;&#x2606;&#x2605;&#x2605;&#x2605';
+      break;
+    case 4:
+      stars = '&#x2606;&#x2605;&#x2605;&#x2605;&#x2605';
+      break;
+    case 5:
+      stars = '&#x2605;&#x2605;&#x2605;&#x2605;&#x2605';
+      break;
+    default:
+      stars = 'N/A';
+    }
 
     if(obj.expanded) {
       return `
@@ -13,7 +33,7 @@ const bookmarkItem = function(){
         <button id="delete">x</button>
         <div data-bookmark-id="${obj.id}" class="bookmark-expand">
           <h3>${obj.title}</h3>
-          <p class="rating">Rating: ${obj.rating = obj.rating ? obj.rating : 'None provided'}</p>
+          <p class="rating">${stars}</p>
           <p class="desc" placeholder="No description provided" disabled>${obj.desc = obj.desc ? obj.desc : 'No description provided'}</p>
           <a href="${obj.url}">Visit Site</a>
         </div>
@@ -26,7 +46,7 @@ const bookmarkItem = function(){
       <button id="delete">x</button>
       <div data-bookmark-id="${obj.id}" class="bookmark-collapse">
         <h3>${obj.title}</h3>
-        <p>Rating: ${obj.rating = obj.rating ? obj.rating : 'N/A'}</p>
+        <p>${stars}</p>
       </div>
     </li>
   `;
